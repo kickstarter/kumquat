00be3fa63c65679cf1606cfc9ef655671c6c5873
Logan McDonald <loganmcdona11@gmail.com>
2017-07-26 14:20:17 -0400

    rails 5 compatibility on 0.0.3

--- config/initializers/rmd_template_handler.rb
+++ config/initializers/rmd_template_handler.rb
@@ -2,7 +2,7 @@ module ActionView
   module Template::Handlers
     class RMarkdown
       class_attribute :default_format
-      self.default_format = Mime::HTML
+      self.default_format = Mime[:HTML]
 
       def call(template)
         k = Kumquat::Knit2HTML.new(Rails.root.join(template.inspect))
